74dbc25513dbfe972d07c3fc68c2ae2593ac3d7f
Dalton Stegner <dmstegner@gmail.com>
2023-12-01 21:17:33 -0500

    puzzle structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::stdin;
+
+use puzzles::day1;
+
+mod puzzles;
+
+fn main() -> Result<(), std::io::Error> {
+    println!("Hello, which day and puzzle would you like to test");
+    let mut day = String::new();
+    stdin().read_line(&mut day)?;
+
+    let mut puzzle = String::new();
+    stdin().read_line(&mut puzzle)?;
+
+    let day_number = day.trim_end().parse::<i32>().unwrap();
+    let puzzle_number = puzzle.trim_end().parse::<i32>().unwrap();
+    println!("day: {}", day);
+    println!("puzzle: {}", puzzle);
+
+    find_match_day_puzzle(day_number, puzzle_number);
+
+    Ok(())
+}
+
+fn find_match_day_puzzle(day: i32, puzzle: i32) {
+    match day {
+        1 => switch_puzzles(puzzle, &day1::puzzle_one, &day1::puzzle_two),
+        _ => (),
+    }
+}
+
+fn switch_puzzles(choice: i32, f1: &dyn Fn(), f2: &dyn Fn()) {
+    match choice {
+        1 => f1(),
+        2 => f2(),
+        _ => (),
+    }
 }
---
+++ src/puzzles/day1.rs
@@ -0,0 +1,6 @@
+pub fn puzzle_one() {
+    println!("running day one puzzle one")
+}
+pub fn puzzle_two() {
+    println!("running day one puzzle two")
+}
